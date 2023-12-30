7642cc9c58c90b37c5271b6934ed801d63b91893
Heinerion <13521176+Heinerion@users.noreply.github.com>
2023-12-30 19:53:12 +0100

    Cast first knowledge into unittest

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void randomItemLoosesSellInPerUpdate() {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo, -1, 0", String.valueOf(app.items[0]));
+        assertEquals(-1, items[0].sellIn);
     }
 
+    @Test
+    void sulfurasDoesNotLooseSellInValue() {
+        Item[] items = new Item[]{new Item(Item.SULFURAS, 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, items[0].sellIn);
+    }
 }
