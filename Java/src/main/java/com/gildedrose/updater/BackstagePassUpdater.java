81b4a44a3740f728ed3491265ae5148ec4c4d7b3
Heinerion <13521176+Heinerion@users.noreply.github.com>
2023-12-30 22:33:46 +0100

    Refactor BackstagePassUpdater

--- Java/src/main/java/com/gildedrose/updater/BackstagePassUpdater.java
+++ Java/src/main/java/com/gildedrose/updater/BackstagePassUpdater.java
@@ -5,21 +5,26 @@ import com.gildedrose.Item;
 final class BackstagePassUpdater implements ItemUpdater {
     public int determineNewQuality(Item item) {
         int quality = item.quality;
-        if (quality < 50) {
-            quality = quality + 1;
-
-            if (item.sellIn < 11) {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
-
-            if (item.sellIn < 6) {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
+        if (quality >= 50) {
+            return quality;
         }
-        return quality;
+
+        return quality + determineQualityBonus(quality, item.sellIn);
+    }
+
+    private static int determineQualityBonus(int quality, int sellIn) {
+        int bonus = 1;
+
+        if (sellIn < 11
+            && quality + bonus < 50) {
+            bonus++;
+        }
+
+        if (sellIn < 6
+            && quality + bonus < 50) {
+            bonus++;
+        }
+
+        return bonus;
     }
 }
