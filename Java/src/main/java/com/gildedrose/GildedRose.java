89525bcaea42ef7cac4be2fb93249e140dad033d
Heinerion <13521176+Heinerion@users.noreply.github.com>
2023-12-30 20:02:57 +0100

    Change all conditions to be positive

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -20,14 +20,8 @@ class GildedRose {
     }
 
     private static void handleWhatever(Item item) {
-        if (!item.isAgedBrie()
-            && !item.isBackstagePasses()) {
-            if (item.quality > 0) {
-                if (!item.isSulfuras()) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if (item.isAgedBrie()
+            || item.isBackstagePasses()) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
@@ -45,30 +39,43 @@ class GildedRose {
                     }
                 }
             }
+        } else {
+            if (item.quality > 0) {
+                if (item.isSulfuras()) {
+                    return;
+                }
+
+                item.quality = item.quality - 1;
+            }
         }
     }
 
     private static void handleSulfurasSellIn(Item item) {
-        if (!item.isSulfuras()) {
-            item.sellIn = item.sellIn - 1;
+        if (item.isSulfuras()) {
+            return;
         }
+
+        item.sellIn = item.sellIn - 1;
     }
 
     private static void handleNegativeSellIn(Item item) {
-        if (item.sellIn < 0) {
-            if (!item.isAgedBrie()) {
-                if (!item.isBackstagePasses()) {
-                    if (item.quality > 0) {
-                        if (!item.isSulfuras()) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
+        if (item.sellIn >= 0) {
+            return;
+        }
+
+        if (item.isAgedBrie()) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        } else {
+            if (item.isBackstagePasses()) {
+                item.quality = item.quality - item.quality;
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                if (item.quality > 0) {
+                    if (item.isSulfuras()) {
+                        return;
+                    }
+                    item.quality = item.quality - 1;
                 }
             }
         }
